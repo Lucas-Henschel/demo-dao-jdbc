96e1de1a108cc520e49cfb7deb61d541a2f03d97
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-10-01 17:05:08 -0300

    delete department implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -9,6 +10,8 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("=== TEST 1: department findById ===");
@@ -21,6 +24,15 @@ public class Program2 {
 		for (Department obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: department delete ===");
+		System.out.println("Enter id for delete department: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJBDC.java
+++ src/model/dao/impl/DepartmentDaoJBDC.java
@@ -27,7 +27,18 @@ public class DepartmentDaoJBDC implements DepartmentDao {
 	}
 
 	@Override
-	public void deleteById(Integer id) {		
+	public void deleteById(Integer id) {	
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("DELETE FROM department "
+					+ " WHERE Id = ? ");
+			
+			st.setInt(1, id);
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 
 	@Override
